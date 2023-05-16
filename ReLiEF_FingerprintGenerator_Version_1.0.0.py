977a2b51f828806940e2d86e6370f18cb1523ce2
bmsamudio <bmsamudio@users.noreply.github.com>
2023-05-15 19:43:43 -0700

    Update ReLiEF_FingerprintGenerator_Version_1.0.0.py

--- ReLiEF_FingerprintGenerator_Version_1.0.0.py
+++ ReLiEF_FingerprintGenerator_Version_1.0.0.py
@@ -1,3 +1,29 @@
+##############################################
+# This script creates Receptor Ligand surfacE Feature (ReLiEF) fingerprints from a directory of *.wrl files corresponding to the sixty conformations belonging to Abl protein kinase and probed by nuclear magnetic resonance
+# as detailed in the following publication:
+# Xie, T.; Saleh, T.; Rossi, P.; Kalodimos, C. G.; Conformational states dynamically populated by a kinase determine its function, Science  2020, 370 (6513).  DOI: 10.1126/science.abc2754
+# 
+# Copyright 2023 Benjamin M. Samudio
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Ben Samudio, May 2023
+# Towards Alleviating Suffering
+###############################################
+
+
+
+
 import re
 import math
 import statistics
